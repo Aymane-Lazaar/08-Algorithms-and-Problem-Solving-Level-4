35205ec86f1295236d3f2505f73ec8ec4829fe30
aymane lazaar <aymanelazaar5@gmail.com>
2026-01-24 04:46:06 +0100

    Refactor number to text conversion for larger numbers

--- 1-Number-To-Text.cpp
+++ 1-Number-To-Text.cpp
@@ -3,9 +3,9 @@
 
 using namespace std;
 
-int ReadNumber()
+long long ReadNumber()
 {
-    int Number;
+    long long Number;
 
     cout << "Enter Number: ";
     cin >> Number;
@@ -13,7 +13,74 @@ int ReadNumber()
     return Number;
 }
 
-string ConvertNumberToText(int Number, string S)
+string NumberToText(long long Number)
+{
+    if (Number == 0)
+    {
+        return "";
+    }
+    if (Number >= 1 && Number <= 19)
+    {
+        string arr[] = {"",
+                        "One", "Two", "Three", "Four", "Five", "Six", "Seven",
+                        "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen",
+                        "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+        return arr[Number] + " ";
+    }
+    if (Number >= 20 && Number <= 99)
+    {
+        string arr[] = {
+            "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+        return arr[Number / 10] + " " + NumberToText(Number % 10);
+    }
+    if (Number >= 100 && Number <= 199)
+    {
+        return "One Hundred " + NumberToText(Number % 100);
+    }
+    if (Number >= 200 && Number <= 999)
+    {
+        return NumberToText(Number / 100) + "Hundreds " + NumberToText(Number % 100);
+    }
+    if (Number >= 1000 && Number <= 1999)
+    {
+        return "One Thousand " + NumberToText(Number % 1000);
+    }
+
+    if (Number >= 2000 && Number <= 999999)
+    {
+        return NumberToText(Number / 1000) + "Thousands " + NumberToText(Number % 1000);
+    }
+    if (Number >= 1000000 && Number <= 1999999)
+    {
+        return "One Million " + NumberToText(Number % 1000000);
+    }
+    if (Number >= 2000000 && Number <= 999999999)
+    {
+        return NumberToText(Number / 1000000) + "Millions " + NumberToText(Number % 1000000);
+    }
+    if (Number >= 1000000000 && Number <= 1999999999)
+    {
+        return "One Billion " + NumberToText(Number % 1000000000);
+    }
+    else
+    {
+        return NumberToText(Number / 1000000000) + "Billions " + NumberToText(Number % 1000000000);
+    }
+}
+
+int main()
+{
+
+    long long Number = ReadNumber();
+    cout << NumberToText(Number);
+
+    return 0;
+}
+
+// My Solution
+/*
+string NumberToText(int Number, string S)
 {
     string ones[] = {
         "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
@@ -53,20 +120,20 @@ string ConvertNumberToText(int Number, string S)
         {
             S += tens[num[0] - '0'] + " ";
             num = num.erase(0, 1);
-            return ConvertNumberToText(stoi(num), S);
+            return NumberToText(stoi(num), S);
         }
     }
     else if (num.length() == 3)
     {
         S += ones[num[0] - '0'] + " hundred ";
         num = num.erase(0, 1);
-        return ConvertNumberToText(stoi(num), S);
+        return NumberToText(stoi(num), S);
     }
     else if (num.length() == 4)
     {
         S += ones[num[0] - '0'] + " thousands ";
         num = num.erase(0, 1);
-        return ConvertNumberToText(stoi(num), S);
+        return NumberToText(stoi(num), S);
     }
     else if (num.length() == 5)
     {
@@ -77,19 +144,19 @@ string ConvertNumberToText(int Number, string S)
         {
             S += ones[sum] + " thousands ";
             num = num.erase(0, 2);
-            return ConvertNumberToText(stoi(num), S);
+            return NumberToText(stoi(num), S);
         }
 
         S += tens[num[0] - '0'] + " " + ones[num[1] - '0'] + " thousands ";
         num = num.erase(0, 2);
-        return ConvertNumberToText(stoi(num), S);
+        return NumberToText(stoi(num), S);
     }
 
     else if (num.length() == 6)
     {
         S += ones[num[0] - '0'] + " hendrend ";
         num = num.erase(0, 1);
-        return ConvertNumberToText(stoi(num), S);
+        return NumberToText(stoi(num), S);
     }
     return S;
 }
@@ -99,7 +166,7 @@ int main()
 
     int Number = ReadNumber();
     string S = "";
-    cout << ConvertNumberToText(Number, S);
+    cout << NumberToText(Number, S);
 
     // int Number = 074;
     // string num = to_string(Number);
@@ -111,5 +178,15 @@ int main()
     // num = num.erase(0, 1);
     // cout<<stoi(num);
 
+    // string ones[] = {
+    //     "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+    //     "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+
+    // string stnumber = "56464";
+
+    // cout<<ones[int(stnumber[1]-48)];
+    // cout<<int('6'-48);
+
     return 0;
 }
+    */
